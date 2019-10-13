1997fdff25b47aefd622eeeff298c332b9fda475
Andrey Pankov <andrey@12devs.com>
2019-10-13 19:24:29 +0300

    supporting docker-compose

--- lib.sh
+++ lib.sh
@@ -1,7 +1,11 @@
 docker_run() {
     cmd=$1
     mapping=$2
-    docker run -it --link $DOCKER_MYSQLD_CONTAINER:mysql $mapping --rm mysql sh -c "exec ${cmd}"
+    network=''
+    if [ $DOCKER_NETWORK ]; then
+        network="--net $DOCKER_NETWORK"
+    fi
+    docker run -it --link $DOCKER_MYSQLD_CONTAINER:mysql $network $mapping --rm mysql sh -c "exec ${cmd}"
 }
 
 run_mysql_cmd() {
@@ -9,6 +13,9 @@ run_mysql_cmd() {
     mapping=$2
     if [ $DOCKER_MYSQLD_CONTAINER ]; then
         docker_run "${cmd}" "${mapping}"
+    elif [ $DOCKER_COMPOSE_SERVICE ]; then
+        fullcmd="docker-compose exec $DOCKER_COMPOSE_SERVICE $cmd"
+        sh -c "$fullcmd"
     else
         bash -c "$cmd"
     fi
